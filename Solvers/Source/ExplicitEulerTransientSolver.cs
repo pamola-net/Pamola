91567916d104137fc7feec35da23507b177707c6
Jean Luca Colombo <jeanlucacolombo@outlook.com>
2020-12-10 11:06:35 -0300

    Implementing ExplicitEulerSolver

--- Solvers/Source/ExplicitEulerTransientSolver.cs
+++ Solvers/Source/ExplicitEulerTransientSolver.cs
@@ -1,12 +1,31 @@
 using System;
 using System.Linq;
 using System.Collections.Generic;
-
+using System.Numerics;
+using Pamola.Transient;
 
 namespace Pamola.Solvers
 {
-    public class ExplicitEulerTransientSolver
+    public class ExplicitEulerTransientSolver : ITransientSolver
     {
-        
+
+        public IEnumerable<IReadOnlyList<Complex>> Solve(
+            IReadOnlyList<Complex> initialState,
+            IReadOnlyList<Func<IReadOnlyList<Complex>, Complex>> derivatives,
+            Func<IReadOnlyList<Complex>, IReadOnlyList<Complex>, double> timeProvider,
+            Action<IReadOnlyList<Complex>> solveSystem
+        )
+        {
+            //TODO: Finish Transient Solver
+            solveSystem(initialState);
+            while (true)
+            {
+                var timeStep = timeProvider(state, stateDerivative)
+
+            }
+            
+            return null;
+        }
     }
 }
+
--- Transient/Source/Abstractions/ITransientSolver.cs
+++ Transient/Source/Abstractions/ITransientSolver.cs
@@ -8,7 +8,9 @@ namespace Pamola.Transient
     {
         IEnumerable<IReadOnlyList<Complex>> Solve(
             IReadOnlyList<Complex> initialState,
-            IReadOnlyList<Func<IReadOnlyList<Complex>, Complex>> derivatives
+            IReadOnlyList<Func<IReadOnlyList<Complex>, Complex>> derivatives,
+            Func<IReadOnlyList<Complex>, IReadOnlyList<Complex>, double> timeProvider,
+            Action<IReadOnlyList<Complex>> solveSystem
         );
     }
 
