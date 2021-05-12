dc56b22d63eed07e40c9df1e96b18cccd0c56143
Jean Luca Colombo <31295051+JeanLColombo@users.noreply.github.com>
2021-05-11 21:54:00 -0300

    Jumper apparently behaving properly.

--- Tests/Core/Components/TestJumper.cs
+++ Tests/Core/Components/TestJumper.cs
@@ -1,43 +1,87 @@
 using Xunit;
 using System.Linq;
+using System.Collections.Generic;
 using Pamola.Components;
 using Pamola.Solvers;
+using Pamola.Solvers.UT.Components;
 
 namespace Pamola.UT.Components
 {
     public class TestJumper
     {
         [Fact]
-        public void TestJumperConnectPositive()
+        public void TestJumperConnectOneSide()
         {
-            var C = SampleCircuit();
-            var J = new Jumper();
+            var J = (Jumper)SampleCircuit().First();
 
-            C.Terminals.ConnectAll();
+            var C = J.GetCircuit();
             
-            var circuit = (J.Positive.ConnectTo(C.Terminals.First())).GetCircuit();
+            C.Solve(new AccordBaseSolver(((IComponent)C).Variables.Select(v => v.Getter()).ToList()));
 
-            circuit.Solve(new AccordBaseSolver(((IComponent)circuit).Variables.Select(v => v.Getter()).ToList()));
+            Assert.Equal(2, C.Terminals.Count);
 
-            /*Assert.Equal(12.0, J.Positive.Node.Voltage.Real);
-            Assert.Equal(0.0, J.Positive.Node.Voltage.Imaginary);
-            Assert.Equal(0.0, J.Positive.Current.Real);
-            Assert.Equal(0.0, J.Positive.Current.Imaginary);*/
-
-            Assert.InRange(C.Terminals.First().Current.Real, 5.99, 6.01);
-            Assert.InRange(C.Terminals.First().Current.Imaginary, -0.01, 0.01);
-            Assert.InRange(C.Terminals.Last().Current.Real,-6.01,-5.99);
-            Assert.InRange(C.Terminals.Last().Current.Imaginary,-0.01, 0.01);
+            Assert.Equal(0.0, J.Positive.Node.Voltage.Real, 4);
+            Assert.Equal(0.0, J.Positive.Node.Voltage.Imaginary, 4);
+            Assert.Equal(0.0, J.Positive.Current.Real, 4);
+            Assert.Equal(0.0, J.Positive.Current.Imaginary, 4);
         }
 
-        private Circuit SampleCircuit()
+        [Fact]
+        public void TestJumperFullyConnected()
         {
-            var V = new IdealDCVoltageSource(12);
-            var R = new IdealResistor(2);
-            
-            R.Positive.ConnectTo(V.Positive);
+            var _sc = SampleCircuit();
+            var J = (Dipole)_sc.First();
+            var R = (Dipole)_sc.Last();
 
-            return R.GetCircuit();
+            J.Negative.ConnectTo(R.Negative);
+            
+            var C = J.GetCircuit();
+            
+            C.Solve(new AccordBaseSolver(((IComponent)C).Variables.Select(v => v.Getter()).ToList()));
+
+            Assert.Equal(0.0, J.Positive.Node.Voltage.Real, 4);
+            Assert.Equal(0.0, J.Positive.Node.Voltage.Imaginary, 4);
+            Assert.Equal(0.0, J.Negative.Node.Voltage.Real, 4);
+            Assert.Equal(0.0, J.Negative.Node.Voltage.Imaginary, 4);
+            Assert.Equal(6.0, J.Negative.Current.Real, 4);
+            Assert.Equal(0.0, J.Negative.Current.Imaginary, 4);
+        }
+
+        [Fact]
+        public void TestShortCircuit()
+        {
+            var _sc = SampleCircuit();
+            var J = (Dipole)_sc.First();
+            var R = (Dipole)_sc.Last();
+
+            J.Negative.ConnectTo(R.Negative);
+
+            var closedCircuit = new Jumper();
+
+            closedCircuit.Positive.ConnectTo(R.Positive);
+            closedCircuit.Negative.ConnectTo(R.Negative);
+
+            var C = J.GetCircuit();            
+            C.Solve(new AccordBaseSolver(((IComponent)C).Variables.Select(v => v.Getter()).ToList()));
+
+            Assert.Equal(0.0, (R.Positive.Node.Voltage - R.Negative.Node.Voltage).Real, 4);
+            Assert.Equal(0.0, (R.Positive.Node.Voltage - R.Negative.Node.Voltage).Imaginary, 4);
+            Assert.Equal(0.0, R.Positive.Current.Real, 4);
+            Assert.Equal(0.0, R.Positive.Current.Imaginary, 4);  
+        }
+
+        private List<IComponent> SampleCircuit()
+        {
+            var V = new IdealDCVoltageSource(12.0);
+            var R = new IdealResistor(2.0);
+            var G = new Ground();
+            var J = new Jumper();
+
+            R.Positive.ConnectTo(V.Positive);
+            V.Negative.ConnectTo(G.Terminal);
+            G.Terminal.Node.ConnectTo(J.Positive);
+
+            return new List<IComponent>(){J, V, R};
         }
     }
 }
