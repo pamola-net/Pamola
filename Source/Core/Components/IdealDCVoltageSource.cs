c471634ea48c468b9369d5908755b1b79f31155a
Jean Luca Colombo <jeanlucacolombo@outlook.com>
2021-05-15 15:10:41 -0300

    Added XLM to all cs files in Core\Components

--- Source/Core/Components/IdealDCVoltageSource.cs
+++ Source/Core/Components/IdealDCVoltageSource.cs
@@ -5,16 +5,31 @@ using System.Numerics;
 
 namespace Pamola.Components
 {
+    /// <summary>
+    /// Describes an Ideal DC voltage source.
+    /// </summary>
     public class IdealDCVoltageSource:
         Dipole
     {
+        /// <summary>
+        /// The <see cref="IdealDCVoltageSource"/> voltage in Volts.
+        /// </summary>
+        /// <value></value>
         public double Voltage { get; private set; }
 
+        /// <summary>
+        /// Creates an instance of <see cref="IdealDCVoltageSource"/>, with given <paramref name="voltage"/>.
+        /// </summary>
+        /// <param name="voltage">The source's voltage.</param>
         public IdealDCVoltageSource(double voltage)
         {
             Voltage = voltage;
         }
 
+        /// <summary>
+        /// The voltage drop through an <see cref="IdealDCVoltageSource"/> is its <see cref="Voltage"/>.
+        /// </summary>
+        /// <returns>The difference between the voltage drop in through its nodes, and its internal <see cref="Voltage"/>.</returns>
         private Complex VoltageDrop()
         {
             if (!Positive.IsConnected() || !Negative.IsConnected()) return new Complex();
@@ -25,8 +40,18 @@ namespace Pamola.Components
 
         }
 
+        /// <summary>
+        /// An <see cref="IdealDCVoltageSource"/> has no variables.
+        /// </summary>
+        /// <typeparam name="Variable"><see cref="Variable"/>.</typeparam>
+        /// <returns>An empty collection of <typeparamref name="Variable"/>.</returns>
         protected override IReadOnlyCollection<Variable> Variables => new List<Variable>();
 
+        /// <summary>
+        /// The only equation associated with an <see cref="IdealDCVoltageSource"/> is its 
+        /// <see cref="VoltageDrop"/>.
+        /// </summary>
+        /// <returns>A read-only collection of functions describing the <see cref="IdealDCVoltageSource"/> behaviour.</returns>
         protected override IReadOnlyCollection<Func<Complex>> DipoleEquations => new List<Func<Complex>>() { VoltageDrop };
 
     }
--- Source/Core/Components/IdealResistor.cs
+++ Source/Core/Components/IdealResistor.cs
@@ -5,11 +5,24 @@ using System.Numerics;
 
 namespace Pamola.Components
 {
+    /// <summary>
+    /// Describes an Ideal Resistor.
+    /// </summary>
     public class IdealResistor : 
         Dipole
     {
+        /// <summary>
+        /// The <see cref="IdealResistor"/> resistance in Ohms.
+        /// </summary>
+        /// <value></value>
         public double Resistance { get; private set; }
 
+        /// <summary>
+        /// Creates an instance of <see cref="IdealResistor"/> with given <paramref name="resistance"/>.
+        /// </summary>
+        /// <param name="resistance">The resistance in Ohms.</param>
+        /// <exception cref="ArgumentOutOfRangeException"> Thrown when <paramref name="resistance"/> is
+        /// negative.</exception>
         public IdealResistor(double resistance)
         {
             if (resistance < 0.0)
@@ -21,6 +34,10 @@ namespace Pamola.Components
             Resistance = resistance; 
         }
 
+        /// <summary>
+        /// Describes Ohm's Law applied to this.
+        /// </summary>
+        /// <returns></returns>
         private Complex OhmsLaw()
         {
             if (!Positive.IsConnected() || !Negative.IsConnected()) return new Complex();
@@ -32,10 +49,19 @@ namespace Pamola.Components
             return V - R * I;
         }   
 
+        /// <summary>
+        /// An <see cref="IdealResistor"/> has no variables.
+        /// </summary>
+        /// <typeparam name="Variable"><see cref="Variable"/>.</typeparam>
+        /// <returns>An empty collection of <typeparamref name="Variable"/>.</returns>
         protected override IReadOnlyCollection<Variable> Variables => new List<Variable>();
 
+        /// <summary>
+        /// The only equation associated with an <see cref="IdealResistor"/> is
+        /// <see cref="OhmsLaw"/>.
+        /// </summary>
+        /// <returns>A read-only collection of functions describing the <see cref="IdealResistor"/> behaviour.</returns>
         protected override IReadOnlyCollection<Func<Complex>> DipoleEquations => new List<Func<Complex>>() { OhmsLaw };
         
-        //TODO: Create an Ideal Voltage Source.
     }
 }
